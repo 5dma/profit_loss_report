fe65905adc3e81092a043bdacc919a15f5c32547
5dma <mark@lautman.net>
2022-06-28 22:15:14 -0400

    Place trees in scrolled windows.

--- view.c
+++ view.c
@@ -39,6 +39,19 @@ GtkWidget *make_window(Data_passer *data_passer) {
     gtk_tree_view_set_model(GTK_TREE_VIEW(tree_view_accounts), GTK_TREE_MODEL(data_passer->accounts_store));
     gtk_tree_view_set_model(GTK_TREE_VIEW(tree_view_reports), GTK_TREE_MODEL(data_passer->accounts_store));
 
+
+    GtkWidget *scrolled_window_tree_view_accounts = gtk_scrolled_window_new(NULL, NULL);
+    gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(scrolled_window_tree_view_accounts), GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
+    gtk_container_add(GTK_CONTAINER(scrolled_window_tree_view_accounts), tree_view_accounts);
+    gtk_widget_set_size_request(scrolled_window_tree_view_accounts, 300, 400); 
+
+    GtkWidget *scrolled_window_tree_view_reports = gtk_scrolled_window_new(NULL, NULL);
+     gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(scrolled_window_tree_view_reports), GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
+    gtk_container_add(GTK_CONTAINER(scrolled_window_tree_view_reports), tree_view_reports);
+    gtk_widget_set_size_request(scrolled_window_tree_view_reports, 300, 400); 
+
+
+
     GtkWidget *btn_save = gtk_button_new_from_icon_name("document-save", GTK_ICON_SIZE_BUTTON);
     GtkWidget *btn_revert = gtk_button_new_from_icon_name("document-revert", GTK_ICON_SIZE_BUTTON);
     GtkWidget *btn_delete = gtk_button_new_from_icon_name("edit-delete", GTK_ICON_SIZE_BUTTON);
@@ -56,8 +69,8 @@ GtkWidget *make_window(Data_passer *data_passer) {
     GtkWidget *grid = gtk_grid_new();
     gtk_grid_attach(GTK_GRID(grid), lbl_accounts, 0, 0, 1, 1);
     gtk_grid_attach(GTK_GRID(grid), lbl_reports, 1, 0, 4, 1);
-    gtk_grid_attach(GTK_GRID(grid), tree_view_accounts, 0, 1, 1, 2);
-    gtk_grid_attach(GTK_GRID(grid), tree_view_reports, 1, 1, 5, 1);
+    gtk_grid_attach(GTK_GRID(grid), scrolled_window_tree_view_accounts, 0, 1, 1, 2);
+    gtk_grid_attach(GTK_GRID(grid), scrolled_window_tree_view_reports, 1, 1, 5, 1);
     gtk_grid_attach(GTK_GRID(grid), btn_save, 1, 2, 1, 1);
     gtk_grid_attach(GTK_GRID(grid), btn_revert, 2, 2, 1, 1);
     gtk_grid_attach(GTK_GRID(grid), btn_delete, 3, 2, 1, 1);
